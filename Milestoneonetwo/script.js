85c9db5be268063a8ac80feaba73eddd293766a7
Ghulam Sarwar Khan <160528570+gsarwarkhan@users.noreply.github.com>
2024-09-16 00:59:37 +0500

    Add files via upload

---
+++ Milestoneonetwo/script.js
@@ -0,0 +1,10 @@
+var toggleButton = document.getElementById('toggleButton');
+var skillsSection = document.getElementById('skills');
+toggleButton.addEventListener('click', function () {
+    if (skillsSection.style.display === 'none') {
+        skillsSection.style.display = 'block';
+    }
+    else {
+        skillsSection.style.display = 'none';
+    }
+});
